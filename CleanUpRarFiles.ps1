14e0e1ccbfd14f001afe158c00efa1ab5121d88e
vCPU <32404802+vcpu79@users.noreply.github.com>
2022-07-13 19:49:33 -0400

    Update with counts and if to only run if count -gt 0

--- CleanUpRarFiles.ps1
+++ CleanUpRarFiles.ps1
@@ -1,12 +1,16 @@
-
-#function to clear dir of rar file types
+  #function to clear dir of rar file types
 function ClearRAR(){
 $rar = ('*.r0*','*.r1*','*.r2*','*.r3*','*.r4*','*.r5*','*.r6*','*.r7*','*.r8*') 
 foreach ($file in $rar){
-   GCI -path $folderpath $file -recurse | foreach { remove-item -path $_.fullname }
-   write-host "$folder DIR - WinRar $file deleted" 
-} 
-} 
+   $count = (GCI -path $folderpath -recurse | where {$_.name -like "$file"} | measure-object).count 
+   write-host "Found $count $file files"
+      if ($count -gt 0) {
+         write-host "Deleting $file files"
+         GCI -path $folderpath $file -recurse | foreach { remove-item -path $_.fullname }
+         write-host "$folder - WinRar $file deleted"
+      }
+   } 
+}   
 
 #Kids Movies Dir
 Write-host "*******Starting Kids Movies DIR cleanup of WinRar files*******"
@@ -23,26 +27,27 @@ ClearRAR($folder,$folderpath)
 #Documentaries Dir
 Write-host "*******Starting Documentaries Movies DIR cleanup of WinRar files*******"
 $folder = "Documentaries Movies DIR"
-$folderpath = 'Z::\Media\Documentaries'
+$folderpath = 'Z:\Media\Documentaries'
 ClearRAR($folder,$folderpath) 
 
 #EDU Dir
 Write-host "*******Starting EDU Movies DIR cleanup of WinRar files*******"
 $folder = "EDU Movies DIR"
-$folderpath = 'Z::\Media\EDU'
+$folderpath = 'Z:\Media\EDU'
 ClearRAR($folder,$folderpath) 
 
 #Stand-ups Dir
 Write-host "*******Starting Stand-ups Movies DIR cleanup of WinRar files*******"
 $folder = "Stand-ups Movies DIR"
-$folderpath = 'Z::\Media\Stand-ups'
+$folderpath = 'Z:\Media\Stand-ups'
 ClearRAR($folder,$folderpath) 
 
 #TV Shows Dir
 Write-host "*******Starting TV Shows DIR cleanup of WinRar files*******"
 $folder = "TV Shows Movies DIR"
-$folderpath = 'Z::\Media\TV Shows'
+$folderpath = 'Z:\Media\TV Shows'
 ClearRAR($folder,$folderpath) 
 
 
 
+ 
---
+++ ClearnUpNfoFiles.ps1
@@ -0,0 +1,53 @@
+  #function to clear dir of nfo file types
+function ClearNFO(){
+$nfo = "*.nfo"
+foreach ($file in $nfo){
+   $count = (GCI -path $folderpath -recurse | where {$_.name -like "$file"} | measure-object).count 
+   write-host "Found $count $file files" 
+      if ($count -gt 0) {
+         write-host "Deleting $file files"
+         GCI -path $folderpath $file -recurse | foreach { remove-item -path $_.fullname }
+         write-host "$folder - nfo $file deleted"
+      }   
+   } 
+}   
+
+#Kids Movies Dir
+Write-host "*******Starting Kids Movies DIR cleanup of nfo files*******"
+$folder = "Kids Movies DIR"
+$folderpath = 'Z:\Media\Kids Movies\'
+ClearNFO($folder,$folderpath) 
+
+#Disney Movies Dir
+Write-host "*******Starting Disney Movies DIR cleanup of nfo files*******"
+$folder = "Disney Movies DIR"
+$folderpath = 'Z:\Media\Disney Movies\'
+ClearNFO($folder,$folderpath) 
+
+#Documentaries Dir
+Write-host "*******Starting Documentaries Movies DIR cleanup of nfo files*******"
+$folder = "Documentaries Movies DIR"
+$folderpath = 'Z:\Media\Documentaries'
+ClearNFO($folder,$folderpath) 
+
+#EDU Dir
+Write-host "*******Starting EDU Movies DIR cleanup of nfo files*******"
+$folder = "EDU Movies DIR"
+$folderpath = 'Z:\Media\EDU'
+ClearNFO($folder,$folderpath) 
+
+#Stand-ups Dir
+Write-host "*******Starting Stand-ups Movies DIR cleanup of nfo files*******"
+$folder = "Stand-ups Movies DIR"
+$folderpath = 'Z:\Media\Stand-ups'
+ClearNFO($folder,$folderpath) 
+
+#TV Shows Dir
+Write-host "*******Starting TV Shows DIR cleanup of nfo files*******"
+$folder = "TV Shows Movies DIR"
+$folderpath = 'Z:\Media\TV Shows'
+ClearNFO($folder,$folderpath) 
+
+
+
+ 
